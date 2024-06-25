8c5446f8dc1b74480015dfc70a73791f1556c398
airlorm <elormnizi@gmail.com>
2024-06-25 12:01:01 +0300

    setup state value for input and displayinput

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Player from './components/Player.jsx';
+
 function App() {
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <Player />
+      <div id="challenges"></div>
     </>
   );
 }
